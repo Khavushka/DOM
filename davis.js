57793bf40b0d9e42c17194a9b6e169ff0bcf0485
Khava <khavushka@gmail.com>
2020-09-13 12:08:17 +0200

    DOM opgaver

--- davis.js
+++ davis.js
@@ -20,7 +20,7 @@ let fillLeft = function (target) {
     img.setAttribute('width', '64');
 
     let h1 = document.createElement('h1'); // create element
-    let txt = document.createTextNode('Always');   // create text
+    let txt = document.createTextNode('Always'); // create text
     h1.appendChild(txt); // put on tree
 
 
@@ -58,44 +58,121 @@ let initialize = function () {
 
 window.addEventListener('load', initialize);
 
-/*
-// function ondragStart
+//Midten
 
-function onDragStart(event) {
-    event   
-        .dataTransfer
-        .setData('text/plain', event. target. id);    
-    event   
-        .currentTarget
-        .style
-        .color = 'yellow';
+let fillCenter = function (target) {
+    let art = target;
+
+    let img = document.createElement('img'); // create element
+    img.setAttribute('src', 'book.png.png');
+    img.setAttribute('alt', 'book icon');
+    img.setAttribute('width', '64');
+
+    let h1 = document.createElement('h1'); // create element
+    h1.style.color = "yellow"; //Farve til center
+    let txt = document.createTextNode('Always'); // create text
+    h1.appendChild(txt); // put on tree
+
+
+    let par = document.createElement('p'); // create element
+    txt = document.createTextNode("You could have four columns here but you won't. You'll have three like everyone else."); // create text
+    par.appendChild(txt); // put onto tree
+    par.style.color = "yellow"; //Farve til center
+
+    art.appendChild(img);
+    art.appendChild(h1);
+    art.appendChild(par);
+    
+}
+
+
+
+let t2 = function (ev) {
+    let target = ev.target || ev.srcElement;
+    let id = target.id;
+    // alert(id);
+    let elm = $(id);
+    if (target.innerHTML !== '') {
+        empty(elm);
+    } else {
+        switch (id) {
+            case 'center': fillCenter(elm); break;
+        }
     }
-*/
+}
 
-/*let sroll = function() {
-    let carousel = setInterval(function() {
+let sinitialize = function () {
+    let center = $('center');
+    fillCenter(center);
+    center.addEventListener('click', t2, false);
+}
+
+window.addEventListener('load', sinitialize);
+
+
+//Højre
+
+let fillRight = function (target) {
+    let art = target;
+
+    let img = document.createElement('img'); // create element
+    img.setAttribute('src', 'book.png.png');
+    img.setAttribute('alt', 'book icon');
+    img.setAttribute('width', '64');
+
+    let h1 = document.createElement('h1'); // create element
+    let txt = document.createTextNode('Always'); // create text
+    h1.appendChild(txt); // put on tree
+
+
+    let par = document.createElement('p'); // create element
+    txt = document.createTextNode("You could have four columns here but you won't. You'll have three like everyone else."); // create text
+    par.appendChild(txt); // put onto tree
+
+    art.appendChild(img);
+    art.appendChild(h1);
+    art.appendChild(par);
+    
+}
+
+
+
+let t3 = function (ev) {
+    let target = ev.target || ev.srcElement;
+    let id = target.id;
+    // alert(id);
+    let elm = $(id);
+    if (target.innerHTML !== '') {
+        empty(elm);
+    } else {
+        switch (id) {
+            case 'right': fillRight(elm); break;
+        }
+    }
+}
+
+let ginitialize = function () {
+    let right = $('right');
+    fillRight(right);
+    right.addEventListener('click', t3, false);
+}
+
+window.addEventListener('load', ginitialize);
+
+
+//Carousel
+
+let sroll = function() {
+    let rev = setInterval(function() {
         let carousel = document.getElementById('carousel');
         if (carousel.firstChild) {
             let obj = carousel.firstChild;
             carousel.removeChild(obj);
             carousel.appendChild(obj);
         }
-        }, 100
+        }, 350
     );
 }
 
+window.addEventListener('load', sroll);
 
-
-window.addEventListener('load', sroll);*/
-
-function handleDragStart(e) {
-    this.style.opacity = '0.4';  // this / e.target is the source node.
-  }
-  
-  var cols = document.querySelectorAll('#columns .column');
-  [].forEach.call(cols, function(col) {
-    col.addEventListener('dragstart', handleDragStart, false);
-  });
-
-
-  
