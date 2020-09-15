ee6116d0d664163e961d373b9335b0709c021d73
Khava <khavushka@gmail.com>
2020-09-15 12:58:25 +0200

    testet og gemt sidste opgave

--- Opgave DE.5.0.js/js.js
+++ Opgave DE.5.0.js/js.js
@@ -1,9 +1,15 @@
 "use strict";
 
-function outputArray(theArray) {
-    let placesToVisit = ["Hawaii",  "Florida", "Washington", "LA", "New York"];
-    placesToVisit.push("Jackson");//push one item last
-    placesToVisit.splice(0, 2);//remove two first items
-document.write(`<h3>USA byer, jeg gerne vil besøge: ${placesToVisit}</h3>`);
+var placesToVisit = ["Hawaii",  "Florida", "Washington", "LA", "New York"];
+    placesToVisit.push("Jackson");//push one item last
+    placesToVisit.splice(0, 2);//remove two first items
+    placesToVisit.sort();//sort element
+    document.write(`<h3>USA byer, jeg gerne vil besøge: ${placesToVisit}</h3>`);
+    document.getElementById("demo").innerHTML = placesToVisit;
 
-}
+function outputArray() {
+    placesToVisit.reverse();
+    document.getElementById("demo").innerHTML = placesToVisit;
+};
+
+
