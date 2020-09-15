4ed9c25c952121f56b1855b299047974c7714b47
Khava <khavushka@gmail.com>
2020-09-15 11:48:29 +0200

    en nye by Jackson til afslytningen af arayet

--- Opgave DE.5.0.js/js.js
+++ Opgave DE.5.0.js/js.js
@@ -1,2 +1,7 @@
 "use strict";
 
+let placesToVisit = ["Hawaii",  "Florida", "Washington", "LA", "New York"];
+placesToVisit.push("Jackson");
+document.write(`<h3>USA byer, jeg gerne vil besøge: ${placesToVisit}</h3>`);
+
+
