5e3d1e886418400e434a6bde41bb5f6c0bd41465
Khava <khavushka@gmail.com>
2020-09-15 12:08:38 +0200

    remove the first two itemsfrom array by using the splice

--- Opgave DE.5.0.js/js.js
+++ Opgave DE.5.0.js/js.js
@@ -1,7 +1,7 @@
 "use strict";
 
 let placesToVisit = ["Hawaii",  "Florida", "Washington", "LA", "New York"];
-placesToVisit.push("Jackson");
+    placesToVisit.push("Jackson");
+    placesToVisit.splice(0, 2);
 document.write(`<h3>USA byer, jeg gerne vil besøge: ${placesToVisit}</h3>`);
-
 
