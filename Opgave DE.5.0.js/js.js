fe2ae398c818ed33d9ae671b6e909f8510c02312
Khava <khavushka@gmail.com>
2020-09-15 12:24:33 +0200

    tilføjet function

--- Opgave DE.5.0.js/js.js
+++ Opgave DE.5.0.js/js.js
@@ -1,7 +1,9 @@
 "use strict";
 
-let placesToVisit = ["Hawaii",  "Florida", "Washington", "LA", "New York"];
-    placesToVisit.push("Jackson");
-    placesToVisit.splice(0, 2);
+function outputArray(theArray) {
+    let placesToVisit = ["Hawaii",  "Florida", "Washington", "LA", "New York"];
+    placesToVisit.push("Jackson");//push one item last
+    placesToVisit.splice(0, 2);//remove two first items
 document.write(`<h3>USA byer, jeg gerne vil besøge: ${placesToVisit}</h3>`);
 
+}
